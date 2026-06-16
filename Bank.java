f4581b7250e1fc9af3ef42d3c24a5c0928c904e7
Jogendra <jogendrasinghkushwaha9@gmail.com>
2026-06-16 18:33:26 +0530

    add oop concept in Bank

--- Bank.java
+++ Bank.java
@@ -1,6 +1,116 @@
 import java.util.Scanner;
 import java.sql.*;
+class BankOperation {
+    connection con=new connection();
+    Scanner sc=new Scanner(System.in);
+    void OpenAccount(){
+        sc.nextLine();
+        System.out.println("Enter Account holder name : ");
+        con.accountHolderName=sc.nextLine();
+        System.out.println("Enter Deposit Amount : ");
+        con.tmpamount=sc.nextDouble();
+        try{
+            con.n=con.st.executeUpdate("insert into account(name,balance) values('"+con.accountHolderName +"',"+con.tmpamount+")");
+            if(con.n>0)
+            {
+                System.out.println("Account Opened Succefully");
+            }
+            else{
+                System.out.println("Account Opened UnSuccefully");
+            }
+        }
+        catch(Exception e){
+            System.out.println(e);
+        }
+    }
 
+    void ShowDetail(){
+        sc.nextLine();
+        System.out.println("Enter Account Holder Name : ");
+        con.accountHolderName=sc.nextLine();
+        try{
+            con.rs=con.st.executeQuery("select * from account where name='"+con.accountHolderName+"'");
+            if(con.rs.next())
+            {
+                System.out.println(con.rs.getInt(1)+" "+con.rs.getString(2)+" "+con.rs.getDouble(3));
+            }
+            else{
+                System.out.println("Account does not exist");
+            }
+        }
+        catch(Exception e){
+            System.out.println(e);
+        }
+    }
+
+    void Deposit(){
+        sc.nextLine();
+        System.out.println("Enter Account Holder Name : ");
+        con.accountHolderName=sc.nextLine();
+        System.out.println("Enter Amount of Deposit : ");
+        con.tmpamount=sc.nextDouble();
+        try{
+            con.n=con.st.executeUpdate("update account set balance =balance+"+con.tmpamount+" where name='"+con.accountHolderName+"'");
+            if(con.n>0)
+            {
+                System.out.println("Amount Added Successfully");
+            }
+            else{
+                System.out.println("Amount Added UnSuccefully");
+            }
+        }
+        catch(Exception e){
+            System.out.println(e);
+        }
+    }
+
+    void WithDraw(){
+        sc.nextLine();
+        System.out.println("Enter Account Holder Name : ");
+        con.accountHolderName=sc.nextLine();
+        System.out.println("Enter withdraw Amount : ");
+        con.tmpamount=sc.nextDouble();
+        try{
+            con.rs=con.st.executeQuery("select * from account where name='"+con.accountHolderName+"'");
+            if(con.rs.next()){
+                if(con.rs.getDouble(3)>=con.tmpamount)
+                {
+                    con.st.executeUpdate("update account set balance =balance-"+con.tmpamount+" where name='"+con.accountHolderName+"'");
+                    System.out.println("Amount withdraw Successfully");
+                }
+                else{
+                    System.out.println("Balance is not available");
+                }
+            }
+            else{
+                System.out.println("Amount withdraw UnSuccefully");
+            }
+        }
+        catch(Exception e){
+            System.out.println(e);
+        }
+    }
+
+    void DeleteAccount(){
+        sc.nextLine();
+        System.out.println("Enter Account Holder Name : ");
+        con.accountHolderName=sc.nextLine();
+        try{
+            con.n=con.st.executeUpdate("delete from account where name='"+con.accountHolderName+"'");
+            if(con.n>0)
+            {
+                System.out.println("Acount deleted Successfully");
+            }
+            else{
+                System.out.println("Account deleted UnSuccefully");
+            }
+        }
+        catch(Exception e){
+            System.out.println(e);
+        }
+    }
+    
+}
 class connection{
     Connection conn;
     Statement st;
@@ -22,6 +132,7 @@ public class Bank{
     public static void main(String[] args){
         Scanner sc=new Scanner(System.in);
         connection con=new connection();
+        BankOperation bo=new BankOperation();
         while(true)
         {
             System.out.println("Bank Management System\n");
@@ -35,106 +146,19 @@ public class Bank{
             con.choice=sc.nextInt();
             switch (con.choice) {
                 case 1:
-                    sc.nextLine();
-                    System.out.println("Enter Account holder name : ");
-                    con.accountHolderName=sc.nextLine();
-                    System.out.println("Enter Deposit Amount : ");
-                    con.tmpamount=sc.nextDouble();
-                    try{
-                        con.n=con.st.executeUpdate("insert into account(name,balance) values('"+con.accountHolderName +"',"+con.tmpamount+")");
-                        if(con.n>0)
-                        {
-                            System.out.println("Account Opened Succefully");
-                        }
-                        else{
-                            System.out.println("Account Opened UnSuccefully");
-                        }
-                    }
-                    catch(Exception e){
-                        System.out.println(e);
-                    }
+                    bo.OpenAccount();
                     break;
                 case 2:
-                    sc.nextLine();
-                    System.out.println("Enter Account Holder Name : ");
-                    con.accountHolderName=sc.nextLine();
-                    try{
-                        con.rs=con.st.executeQuery("select * from account where name='"+con.accountHolderName+"'");
-                        if(con.rs.next())
-                        {
-                            System.out.println(con.rs.getInt(1)+" "+con.rs.getString(2)+" "+con.rs.getDouble(3));
-                        }
-                        else{
-                            System.out.println("Account does not exist");
-                        }
-                    }
-                    catch(Exception e){
-                        System.out.println(e);
-                    }
+                    bo.ShowDetail();;
                     break;
                 case 3:
-                    sc.nextLine();
-                    System.out.println("Enter Account Holder Name : ");
-                    con.accountHolderName=sc.nextLine();
-                    System.out.println("Enter Amount of Deposit : ");
-                    con.tmpamount=sc.nextDouble();
-                    try{
-                        con.n=con.st.executeUpdate("update account set balance =balance+"+con.tmpamount+" where name='"+con.accountHolderName+"'");
-                        if(con.n>0)
-                        {
-                            System.out.println("Amount Added Successfully");
-                        }
-                        else{
-                            System.out.println("Amount Added UnSuccefully");
-                        }
-                    }
-                    catch(Exception e){
-                        System.out.println(e);
-                    }
+                    bo.Deposit();
                     break;
                 case 4:
-                    sc.nextLine();
-                    System.out.println("Enter Account Holder Name : ");
-                    con.accountHolderName=sc.nextLine();
-                    System.out.println("Enter withdraw Amount : ");
-                    con.tmpamount=sc.nextDouble();
-                    try{
-                        con.rs=con.st.executeQuery("select * from account where name='"+con.accountHolderName+"'");
-                        if(con.rs.next()){
-                            if(con.rs.getDouble(3)>=con.tmpamount)
-                            {
-                                con.st.executeUpdate("update account set balance =balance-"+con.tmpamount+" where name='"+con.accountHolderName+"'");
-                                System.out.println("Amount withdraw Successfully");
-                            }
-                            else{
-                                System.out.println("Balance is not available");
-                            }
-                        }
-                        else{
-                            System.out.println("Amount withdraw UnSuccefully");
-                        }
-                    }
-                    catch(Exception e){
-                        System.out.println(e);
-                    }
+                    bo.WithDraw();
                     break;
                 case 5:
-                    sc.nextLine();
-                    System.out.println("Enter Account Holder Name : ");
-                    con.accountHolderName=sc.nextLine();
-                    try{
-                        con.n=con.st.executeUpdate("delete from account where name='"+con.accountHolderName+"'");
-                        if(con.n>0)
-                        {
-                            System.out.println("Acount deleted Successfully");
-                        }
-                        else{
-                            System.out.println("Account deleted UnSuccefully");
-                        }
-                    }
-                    catch(Exception e){
-                        System.out.println(e);
-                    }
+                   bo.DeleteAccount();;
                     break;
                 case 6:
                     System.exit(0);
